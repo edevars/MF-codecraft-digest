245ea9caf95f69a8ed1c74c1edf07746210252f5
edevars <e.devars.dev@gmail.com>
2024-02-15 12:51:00 -0600

    feat: adding styles in sass module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import { HomeHeader } from "app/components/home/HomeHeader"
 import { SuscriptionForm } from "app/components/home/SuscriptionForm"
 import { SuccessMessage } from 'app/components/home/SuccessMessage';
+import styles from 'app/styles/Home.module.sass'
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home(){
@@ -11,12 +12,7 @@ export default function Home(){
   const [isSubscribed, setIsSuscribed] = useState(false)
 
   return(
-    <main style={{
-      display: "block",
-      margin: '0 auto',
-      padding: 12,
-      maxWidth: "420px"
-    }}>
+    <main className={styles.Home}>
       <HomeHeader isSuscribed={isSubscribed}/>
       {
         isSubscribed ? <SuccessMessage /> : <SuscriptionForm setIsSuscribed={setIsSuscribed}/>
